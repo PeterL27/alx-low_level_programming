6649550340fef1783ebba908f80e2ddce83ce125
PeterL27 <Peter.lourens@yahoo.com>
2022-09-13 09:14:39 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
+
