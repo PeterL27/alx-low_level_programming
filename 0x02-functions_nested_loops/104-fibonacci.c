3343f156bfa1ab04a8778cb4d177bdf53e9c1563
PeterL27 <Peter.lourens@yahoo.com>
2022-09-14 09:50:36 -0700

    Task 12

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the first 98 Finonacci numbers
+ * starting with 1 and 2
+ * followed by new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	unsigned long int i, j, k, j1, j2, k1, k2;
+
+	j = 1;
+	k = 2;
+
+	printf("%lu. j);
