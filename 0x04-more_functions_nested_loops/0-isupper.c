7e0a47ed4d37b88eedc6910511b4949ddc57a8a2
PeterL27 <Peter.lourens@yahoo.com>
2022-09-15 04:06:23 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,15 +7,7 @@
  * Return: give 1 or 0
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-
-	else
-	{
-		return (0);
-	}
+	return (c >= 'A' && c <= 'Z');
 }
