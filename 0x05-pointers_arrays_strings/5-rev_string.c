2346b6c87c383ac316360c82ed562cb217820771
PeterL27 <Peter.lourens@yahoo.com>
2022-09-20 07:45:51 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,4 +6,27 @@
  */
 
 void rev_string(char *s)
+{
+	int i, c, j;
+	char *a, aux;
 
+	a = s;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (j = 1; j < c; j++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
+	}
+}
