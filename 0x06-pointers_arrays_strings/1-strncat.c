a1060d70510a6d0304511a6f4f154b4c0341d6e2
PeterL27 <Peter.lourens@yahoo.com>
2022-09-21 09:20:55 -0700

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,9 +2,9 @@
 
 /**
  * _strncat - concatenates two strings but add inputted number in bytes
- * @dest: string to be appended
+ * @dest: string to be appended upon
  * @src: string to be completed at end of dest
- * @n: integer paramater
+ * @n: integer paramater to compare index to
  *
  * Return: returns to new concatenated string
  */
