5c52aa6fac173a01a7acc34bd2bf410cc2da0f1c
PeterL27 <Peter.lourens@yahoo.com>
2022-09-22 01:51:03 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,8 +10,8 @@
 char *leet(char *x)
 {
 	int a = 0, b, l = 5;
-	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
-	char trw[5] = {'4', '3', '0', '7', '1'};
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
 
 	while (x[a])
 	{
@@ -19,11 +19,18 @@ char *leet(char *x)
 
 		while (b < l)
 		{
-			if (x[a] == tr[b] || x[a] - 32 == tr[b])
-				x[a] == trw[b];
+			if (x[a] == r[b] || x[a] - 32 == r[b])
+			{
+				x[a] = n[b];
+			}
+
 			b++;
 		}
+
 		a++;
+
 	}
+
 	return (x);
+}
 
