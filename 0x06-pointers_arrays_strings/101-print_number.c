a17f4414b383456bb68482554a14f7d5af767abe
PeterL27 <Peter.lourens@yahoo.com>
2022-09-22 02:02:25 -0700

    Task 9 fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,6 +3,8 @@
 /**
  * print_number - prints an integer
  * @n: integer to be printed
+ *
+ * Return: nothing
  */
 
 void print_number(int n)
@@ -19,4 +21,6 @@ void print_number(int n)
 
 	if (i / 10)
 		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
 }
