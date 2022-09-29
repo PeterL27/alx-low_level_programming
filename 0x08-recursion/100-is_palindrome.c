2c99e0981f360b906e134cdc8767cfd5053fd8d3
PeterL27 <Peter.lourens@yahoo.com>
2022-09-29 11:47:43 -0700

    Task 7 fix

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -25,7 +25,7 @@ int _strlen_recursion(char *s)
  */
 int check_palindrome(char *s, int n1, int n2)
 {
-	if (*(s + n1) == *s(s + n2))
+	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
