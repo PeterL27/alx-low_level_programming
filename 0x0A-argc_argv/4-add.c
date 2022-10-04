129338b35e9e44daf99231731f4400597e180a41
PeterL27 <Peter.lourens@yahoo.com>
2022-10-04 13:50:29 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	int inc = 0, 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int j;
+			char *str;
+
+			str = argv[i];
+			for (j = 0; str[j] != '\0'; j++)
+				if (str[j] < 48 || str[j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		inc += atoi(argv[i]);
+	}
+	printf("%d\n", inc);
+	return (0);
+}
