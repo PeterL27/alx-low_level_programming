ea37ad6237b0e1c4121eaa4cc20f960113f320d7
PeterL27 <Peter.lourens@yahoo.com>
2022-10-10 08:31:30 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include <stdlib>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: first member
+ * @age second member
+ * @owner: third member
+ *
+ * Return: NULL if fail
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, len, lenO;
+	struct dog *n_dog = NULL;
+
+	len = 0;
+	while (name[len] != '\0')
+		len++;
+
+	lenO = 0;
+	while (owner[lenO] != '\0')
+		leno++;
+
+	n_dog = malloc(sizeof(struct dog));
+	if (n_dog == NULL)
+	{
+		free(n_dog);
+		return (NULL);
+	}
+	n_dog->name = malloc(lenN + 1);
+	if (n_dog->name == NULL)
+	{
+		free(n_dog->name);
+		free(n_dog);
+		return (NULL);
+	}
+	n_dog->owner = malloc(lenO + 1);
+	if (n_dog->owner == NULL)
+	{
+		free(n_dog->name);
+		free(n_dog->owner);
+		free(n_dog);
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+		n_dog->name[i] = name[i];
+	for (i = 0; i <= lenO; i++)
+		n_dog->owner[i] = owner[i];
+	n_dog->age = age;
+	return (n_dog);
+}
