460d54618b8691e12b9509dcb0891490b0c1a254
PeterL27 <Peter.lourens@yahoo.com>
2022-10-13 04:51:40 -0700

    Task 1 hot fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i < n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
-	print("\n");
+	printf("\n");
 
 	va_end(numb);
 }
