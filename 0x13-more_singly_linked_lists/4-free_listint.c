7e6dfc1d3b26a79fc134e297cfeeb640e8173706
PeterL27 <peter.lourens@yahoo.com>
2022-10-25 10:06:56 -0700

    Task 4 fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,12 +9,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
-	while (head != NULL)
+	while (head)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
