9fd118f37abdb2a6a04a275a8aa3f09954d2edd2
PeterL27 <peter.lourens@yahoo.com>
2022-10-31 12:26:51 -0700

    Task 0 fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,28 +13,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	int fd;
+	ssize_t nrd, nwr;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buffer)
 		return (0);
-	}
+
+	nrd = read(fd, buffer, letters);
+	nwr = write(STDOUT_FILENO, buffer, nrd);
+
+	close(fd);
 
 	free(buffer);
-	close(o);
 
-	return (w);
+	return (nwr);
 }
